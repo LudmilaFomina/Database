7242cf448127d9effde241881b427caaf5dde120
LudmilaFomina <113899794+LudmilaFomina@users.noreply.github.com>
2024-01-09 12:48:45 +0100

    Update database.py
    removed the description

--- database.py
+++ database.py
@@ -1,15 +1,3 @@
-"""
-Database:
-Name - Date of birth.
-
-Available commands:
-init - create a new database
-add 'Name Date of Birth' - add 'Name Date of Birth'
-read 'Name' - show data for 'Name'
-readall - show the entire database
-drop 'Name' - delete data by 'Name'
-"""
-
 ################################################
 # imports
 ################################################
